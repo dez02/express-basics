b35363662b702192ac6873ad6591b25c445840f2
Désirée <deutodesiree@gmail.com>
2017-10-29 19:21:52 +0100

    eslint config/set view

--- app/server.js
+++ app/server.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const path = require('path');
+const routes = require('./routes/index');
+const logger = require('morgan');
 
 const app = express();
+app.set('view engine', 'ejs');
+app.set('views', path.resolve(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(logger('dev'));
+app.use(routes);
 
 app.listen(3000, () => {
-    console.log('youhouuu!');
+  console.log('youhouuu!');
 });
